1dc49a4598c425b7f2ba8b8af17c1f80d6190579
hemara0 <54804256+hemara0@users.noreply.github.com>
2022-02-13 21:17:37 +0530

    colors and homescreen commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,9 +3,10 @@ import React from 'react';
 import { Surface,Title } from 'react-native-paper';
 import Feather from 'react-native-vector-icons/Feather';
 import { color } from 'react-native-reanimated';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import Ionic from "react-native-vector-icons/Ionicons";
-
-
+import Colors from '../constants/Colors';
+const IconSize = 30;
 const HomeScreen = () => {
   return (
         <Surface style = {styles.header}>
@@ -16,6 +17,8 @@ const HomeScreen = () => {
               { <TouchableOpacity>
                 <Ionic name = 'filter' size = {IconSize} color = {Colors.black}/>
                 </TouchableOpacity>}
+                </View>
+                <View style = {styles.view, styles.rightView}>
             { <TouchableOpacity>
                 <Ionic name = 'cart' size = {IconSize} color = {Colors.black}/>
                 </TouchableOpacity>}
@@ -28,22 +31,22 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   header:{
     height: 100,
-    elevation: 4,
-    justifyContent: 'space-between',
+    elevation: 4,  
     alignItems: 'center',
     flexDirection: 'row',
-    backgroundColor: 'white'
+    backgroundColor: Colors.white,
 
   },
   view:{
-    flex:1,
+    
     backgroundColor: 'white',
     margin: 30,
     alignItems: 'center',
-    flexDirection: 'row',
+    flexWrap: "wrap"
   },
-  rightView:{
-    justifyContent: 'flex-end'
+  rightview:{
+  justifyContent: 'space-evenly',
+  flex: 1,
   }
 }
 )
