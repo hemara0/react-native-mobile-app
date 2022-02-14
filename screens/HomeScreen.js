34ba39bf8647dccb862ce433910fb4a28ea2227b
hemara0 <54804256+hemara0@users.noreply.github.com>
2022-02-14 11:02:35 +0530

    Homescreen app header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,52 +1,68 @@
-import {StyleSheet, View, Text } from 'react-native';
-import React from 'react';
-import { Surface,Title } from 'react-native-paper';
-import Feather from 'react-native-vector-icons/Feather';
-import { color } from 'react-native-reanimated';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import Ionic from "react-native-vector-icons/Ionicons";
+import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { Badge, Surface, Title } from 'react-native-paper'
+import Feather from 'react-native-vector-icons/Feather'
 import Colors from '../constants/Colors';
-const IconSize = 30;
-const HomeScreen = () => {
-  return (
-        <Surface style = {styles.header}>
-            <View style = {styles.view}>
-              <Title>Rentabook </Title>
-            </View>
-            <View style = {styles.view, styles.rightView}>
-              { <TouchableOpacity>
-                <Ionic name = 'filter' size = {IconSize} color = {Colors.black}/>
-                </TouchableOpacity>}
-                </View>
-                <View style = {styles.view, styles.rightView}>
-            { <TouchableOpacity>
-                <Ionic name = 'cart' size = {IconSize} color = {Colors.black}/>
-                </TouchableOpacity>}
-            </View>
-        </Surface>
-  );
-};
+import Ionic from "react-native-vector-icons/Ionicons";
 
-export default HomeScreen;
-const styles = StyleSheet.create({
-  header:{
-    height: 100,
-    elevation: 4,  
-    alignItems: 'center',
-    flexDirection: 'row',
-    backgroundColor: Colors.white,
+const IconSize = 24;
 
-  },
-  view:{
-    
-    backgroundColor: 'white',
-    margin: 30,
-    alignItems: 'center',
-    flexWrap: "wrap"
-  },
-  rightview:{
-  justifyContent: 'space-evenly',
-  flex: 1,
-  }
+const AppHeader = () => {
+  const RightView = () => (
+			<View style={[styles.view, styles.rightView]}>
+				{<TouchableOpacity style={styles.rowView}>
+					<Ionic name='cart' size={IconSize} color={Colors.black} />
+					{<Badge style={{ position: 'absolute', top: -5, right: -10 }}></Badge>}
+				</TouchableOpacity>}
+				{<TouchableOpacity>
+					<Ionic name='filter' size={IconSize} color = {Colors.black} />
+				</TouchableOpacity>}
+			</View>
+  )
+	const TitleView = () => (
+		<View style={styles.titleView}>
+			<Title style={{ color: 'black' }}>RentaBook</Title>
+		</View>
+	)
+	return (
+		<Surface style={[styles.header, { backgroundColor: 'white' }]}>
+	
+			<TitleView />
+      <RightView />
+		</Surface>
+	)
 }
-)
+
+export default AppHeader
+
+const styles = StyleSheet.create({
+	header: {
+		height: 100,
+		elevation: 8,
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		flexDirection: 'row',
+		backgroundColor: Colors.white,
+	},
+	view: {
+		marginHorizontal: 16,
+		alignItems: 'center',
+		flexDirection: 'row',
+	},
+	titleView: {
+		flex: 1,
+    alignItems: 'flex-start',
+    marginHorizontal: 30,
+    fontSize: 40
+	},
+	rightView: {
+    flex: 1,
+		justifyContent: 'flex-end',
+    justifyContent: 'space-evenly'
+	},
+	rowView: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		marginRight: 10,
+	}
+})
